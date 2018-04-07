206882d887e2ed67f56e37c4362e901929c0ba4e
Kern Ball <kern.ball@yahoo.co.uk>
2018-04-07 18:57:31 +0100

    Skeleton LookAt
    Skeleton now always looks at player.

---
+++ 3D Rogue/Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    public GameObject head;
+
+    private GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player(Clone)");
+	}
+	
+	void Update () {
+        head.transform.LookAt(player.transform);
+	}
+}
